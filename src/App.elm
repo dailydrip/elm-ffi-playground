028869b9ac57a739a00e6922e87e1a406997e355
Josh Adams <josh.rubyist@gmail.com>
2017-02-27 04:45:35 -0600

    Use FFI.async

--- src/App.elm
+++ src/App.elm
@@ -5,13 +5,23 @@ import Html.Attributes exposing (src)
 import FFI
 import Html.Events exposing (onClick)
 import Json.Decode as Decode
+import Json.Encode exposing (Value)
+import Task exposing (Task)
 
 
-getHtml : String -> String
+getHtml : String -> Task Value Value
 getHtml nodeId =
-    FFI.sync "return window.getHtml(_0);" [ FFI.asIs nodeId ]
-        |> Decode.decodeValue Decode.string
-        |> Result.withDefault ""
+    FFI.async
+        """
+        setTimeout(function(){
+            try {
+                callback(_succeed(window.getHtml(_0)));
+            } catch (exception) {
+                callback(_fail(exception.message));
+            }
+        }, 2000);
+        """
+        [ FFI.asIs nodeId ]
 
 
 type alias Model =
@@ -27,13 +37,34 @@ init path =
 
 type Msg
     = Log
+    | GotHtml (Result Value Value)
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Log ->
-            ( { model | message = getHtml "root" }, Cmd.none )
+            model ! [ Task.attempt GotHtml (getHtml "root") ]
+
+        GotHtml result ->
+            case result of
+                Ok response ->
+                    let
+                        message =
+                            response
+                                |> Decode.decodeValue Decode.string
+                                |> Result.withDefault ""
+                    in
+                        { model | message = message } ! []
+
+                Err error ->
+                    let
+                        message =
+                            error
+                                |> Decode.decodeValue Decode.string
+                                |> Result.withDefault ""
+                    in
+                        { model | message = message } ! []
 
 
 view : Model -> Html Msg
